b9b34d4ab472cf9b7932d08189ec2de3056c8ac2
Angelo <an9el0wu@gmail.com>
2022-11-09 20:24:43 +0800

    MaxArrayDeque.java done

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,50 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    private Comparator<T> defaultComparator;
+
+    public MaxArrayDeque(Comparator<T> c){
+        super();
+        defaultComparator = c;
+    }
+
+    public T max(){
+        if (size() == 0){
+            return null;
+        }
+        T maxItem = get(0);
+        for (T i : this){
+            if (defaultComparator.compare(maxItem, i) < 0){
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+    public T max(Comparator<T> c){
+        if (size() == 0){
+            return null;
+        }
+        T maxItem = get(0);
+        for (T i : this){
+            if (c.compare(maxItem, i) > 0){
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+
+    /**My Integer Comparator. */
+    private static class intComparator implements Comparator<Integer>{
+        @Override
+        public int compare(Integer a, Integer b){
+            return a - b;
+        }
+    }
+
+    public static Comparator<Integer> getIntComparator(){
+        return new intComparator();
+    }
+
+}
